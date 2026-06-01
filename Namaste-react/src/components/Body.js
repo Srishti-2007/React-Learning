1fde9928df6c97a3a5ae6f00de9f740bebe855cb
Srishti <ssrishti8968@gmail.com>
2026-06-01 21:22:37 +0530

    complete 5th lec

--- Namaste-react/src/components/Body.js
+++ Namaste-react/src/components/Body.js
@@ -1,11 +1,21 @@
 import RestaurentCard from "./RestaurentCard";
-
+import { useState } from "react";
 import resList from "../utils/mockData";
  
 const Body = () => {
+  const [Listofrest,seListofrest]=useState(resList);
   return (
     <div className="body">
-      <div className="search">search</div>
+      <div className="filter">
+        <button className="filter-btn" 
+        onClick={()=>{
+          const filterlist=Listofrest.filter(
+            (restaurant)=>restaurant.data.avgRating > 4
+          );
+          seListofrest(filterlist);
+        }}>
+          Top Rated Restaurant</button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurentCard
